1f85d0aaeaf7d1c657cfe3bc89383959f46712c0
Michael Rios <michael.rios093@gmail.com>
2020-11-11 21:58:40 -0500

    data layer seems ok

---
+++ db.js
@@ -0,0 +1,71 @@
+const { INTEGER, DATE } = require('sequelize');
+const Sequelize = require('sequelize');
+const { STRING } = Sequelize;
+
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/acme_people_places_db')
+
+const People = conn.define('people', {
+    name: {
+        type: STRING,
+        allowNull: false,
+        unique: true
+    }
+
+});
+
+const Place = conn.define('place', {
+    name: {
+        type: STRING,
+        allowNull: false,
+        unique: true
+    }
+
+});
+
+const Thing = conn.define('thing', {
+    name: {
+        type: STRING,
+        allowNull: false,
+        unique: true
+    }
+
+});
+
+const Purchase = conn.define('purchase', {
+    count: {
+        type: INTEGER,
+        allowNull: false,
+        //unique: true
+    }
+    ,
+    date: {
+        type: DATE
+        //allowNull: false,
+        //unique: true
+    }
+    
+});
+
+Purchase.belongsTo(People);
+Purchase.belongsTo(Place);
+Purchase.belongsTo(Thing);
+
+const syncAndSeed = async()=> {
+    await conn.sync({ force: true});
+    const [lucy, moe, larry] = await Promise.all(
+        ['lucy', 'moe', 'larry'].map(name => People.create({name}))
+    );
+    const [NYC, Chicago, LA, Dallas] = await Promise.all(
+        ['NYC', 'Chicago', 'LA', 'Dallas'].map(name => Place.create({name}))
+    );
+    const [foo, bar, bazz, quq] = await Promise.all(
+        ['foo', 'bar', 'bazz', 'quq'].map(name => Thing.create({name}))
+    );
+    await Purchase.create({personId: 1, placeId: 3, thingId: 1, count: 3, date: "10-31-2020"});
+    await Purchase.create({personId: 3, placeId: 2, thingId: 2, count: 3, date: "11-06-2019"});    
+    await Purchase.create({personId: 2, placeId: 2, thingId: 3, count: 3, date: "03-15-2020"});    
+}
+
+module.exports = {
+    syncAndSeed
+};
---
+++ server.js
@@ -0,0 +1,13 @@
+const { syncAndSeed } = require('./db');
+
+const init = async() => {
+    try {
+    await syncAndSeed()
+    console.log('ready');
+    }
+    catch(ex){
+        console.log(ex);
+    }
+};
+
+init();
